7a57052ff737873d8a7750ee1e1b8825ec07abed
Cassus Adam Banko <banko.adam@gmail.com>
2011-01-21 22:37:59 +0100

    sfAutoconfig alapú konfiguráció

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -18,25 +18,29 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+
   public function setup()
   {
     // We do this here because we chose to put Zend in lib/vendor/Zend.
     // If it is installed system-wide then this isn't necessary to
     // enable Zend Search
     set_include_path(
-      sfConfig::get('sf_lib_dir') .
-        '/vendor' . PATH_SEPARATOR . get_include_path());
+            sfConfig::get('sf_lib_dir') .
+            '/vendor' . PATH_SEPARATOR . get_include_path());
     // ORDER IS SIGNIFICANT. sfDoctrinePlugin logically comes first followed by sfDoctrineGuardPlugin.
     // apostrophePlugin must precede apostropheBlogPlugin. 
     $this->enablePlugins(array(
-      'sfDoctrinePlugin',
-      'sfDoctrineGuardPlugin',
-      'sfDoctrineActAsTaggablePlugin',
-      'sfTaskExtraPlugin',
-      'sfWebBrowserPlugin',
-      'sfFeed2Plugin',
-      'sfSyncContentPlugin',
-      'apostrophePlugin',
-      'apostropheBlogPlugin'));
+        'sfDoctrinePlugin',
+        'sfDoctrineGuardPlugin',
+        'sfDoctrineActAsTaggablePlugin',
+        'sfTaskExtraPlugin',
+        'sfWebBrowserPlugin',
+        'sfFeed2Plugin',
+        'sfSyncContentPlugin',
+        'apostrophePlugin',
+        'apostropheBlogPlugin',
+        'sfAutoconfigPlugin',
+    ));
   }
+
 }
