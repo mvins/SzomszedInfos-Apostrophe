22acf52413418b6e169506ff4a6135b1b52a191e
Cassus Adam Banko <banko.adam@gmail.com>
2011-03-10 09:50:46 +0100

    Eddigi design ötletek és eredmények

---
+++ doc/production-files/forces.mm
@@ -0,0 +1,55 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1280053983423" ID="ID_321687418" MODIFIED="1280054008283" TEXT="http://szomszedinfos.hu FORCES">
+<node COLOR="#669900" CREATED="1280059591534" HGAP="-247" ID="ID_1460194840" MODIFIED="1280060342129" POSITION="right" TEXT="Alap" VSHIFT="-45">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1280059463726" ID="ID_308504029" MODIFIED="1280059557754" TEXT="Felkelti a figyelmemet, ha valaki kil&#xf3;g a sorb&#xf3;l"/>
+<node CREATED="1280054067695" ID="ID_1339739962" MODIFIED="1280054070971" TEXT="Hol vagyok?"/>
+<node CREATED="1280054071279" ID="ID_1450007052" MODIFIED="1280054073371" TEXT="Mi ez?"/>
+<node CREATED="1280054073647" ID="ID_1078539478" MODIFIED="1280058542154" TEXT="Mi&#xe9;rt Szomsz&#xe9;d Inf&#xf3;s a n&#xe9;v?"/>
+<node CREATED="1280054079135" ID="ID_1175389582" MODIFIED="1280054092203" TEXT="Csak 10 m&#xe1;sodpercem van, a l&#xe9;nyeget akarom l&#xe1;tni"/>
+<node CREATED="1280054092767" ID="ID_1937383750" MODIFIED="1280054100363" TEXT="Don&apos;t make me think!"/>
+<node CREATED="1280056171071" ID="ID_1960812739" MODIFIED="1280056179851" TEXT="&#xd6;tletem van, hova &#xed;rjam?"/>
+<node CREATED="1280060069966" ID="ID_77923733" MODIFIED="1280060132762" TEXT="Ha sz&#xe1;m&#xed;t&#xf3;g&#xe9;pes seg&#xed;ts&#xe9;gre van sz&#xfc;ks&#xe9;gem, megtal&#xe1;ljalak titeket a google-el"/>
+<node CREATED="1280060522804" ID="ID_1860904624" MODIFIED="1280060534426" TEXT="A helyes&#xed;r&#xe1;si hib&#xe1;k bizalmatlann&#xe1; tesznek"/>
+</node>
+<node COLOR="#669900" CREATED="1280059632798" HGAP="33" ID="ID_706844753" MODIFIED="1280089255886" POSITION="right" TEXT="&#xdc;gyf&#xe9;l" VSHIFT="-22">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1280054103743" ID="ID_1345907566" MODIFIED="1280054113595" TEXT="Nekem val&#xf3; ez?"/>
+<node CREATED="1280059669230" ID="ID_1293164976" MODIFIED="1280059695658" TEXT="Csak a fontos dolgokra adok ki p&#xe9;nzt."/>
+<node CREATED="1280059745332" ID="ID_713698308" MODIFIED="1280059753162" TEXT="Miben m&#xe1;s ez, mint a t&#xf6;bbi support c&#xe9;g?"/>
+<node CREATED="1280059760142" ID="ID_1620673909" MODIFIED="1280059776650" TEXT="Nem vagyok benne biztos, hogy kell valaki, aki az informatik&#xe1;nkat karbantartja."/>
+<node CREATED="1280059796414" ID="ID_611852706" MODIFIED="1280059832928" TEXT="Szeretem el&#x151;re tudni, mi fog t&#xf6;rt&#xe9;nni, ha megrendelem"/>
+<node CREATED="1280059796414" ID="ID_1799832174" MODIFIED="1280059856042" TEXT="Szeretem el&#x151;re tudni, hogyan fogunk egy&#xfc;tt dolgozni"/>
+<node CREATED="1280058731410" ID="ID_1856252342" MODIFIED="1280058739882" TEXT="Mit kell tennem a megrendel&#xe9;shez?"/>
+<node CREATED="1280058740558" ID="ID_271984997" MODIFIED="1280058775050" TEXT="Hogyan t&#xf6;rt&#xe9;nik a megrendel&#xe9;s?"/>
+<node CREATED="1280059368238" ID="ID_1466611521" MODIFIED="1280059935050" TEXT="&#xc9;rt&#xe9;kelem, ha &#xe9;rdemben meghallgatnak"/>
+<node CREATED="1280059562734" ID="ID_1810001739" MODIFIED="1280059575946" TEXT="Megb&#xed;zhat&#xf3; szolg&#xe1;ltat&#xe1;st szeretn&#xe9;k"/>
+<node CREATED="1280060149246" ID="ID_1224206112" MODIFIED="1280060279786" TEXT="Csak n&#xe9;zel&#x151;d&#xf6;m, tal&#xe1;lok itt valami hasznos dolgot ingyen?"/>
+<node CREATED="1280060280814" ID="ID_1605543880" MODIFIED="1280060296058" TEXT="Hasznos inform&#xe1;ci&#xf3;k&#xe9;rt cser&#xe9;be sz&#xed;vesen megadom az email c&#xed;memet"/>
+<node CREATED="1280060386980" ID="ID_1619122120" MODIFIED="1280060404570" TEXT="Alapvet&#x151;en bizalmatlan vagyok, sz&#xfc;ks&#xe9;gem van valamire, hogy b&#xed;zzak bennetek"/>
+<node CREATED="1280060612430" ID="ID_1486959905" MODIFIED="1280060655786" TEXT="Most nincs sz&#xfc;ks&#xe9;gem r&#xe1;tok, de ha valami eml&#xe9;kezeteset produk&#xe1;ltok, akkor amikor probl&#xe9;m&#xe1;m van, titeket foglak keresni."/>
+<node CREATED="1280089456680" ID="ID_1810934240" MODIFIED="1280089651683" TEXT="Bizalmat &#xe9;breszt bennem, ha l&#xe1;tom, hogy sokan vett&#xe9;k ig&#xe9;nybe a szolg&#xe1;ltat&#xe1;st"/>
+</node>
+<node COLOR="#669900" CREATED="1280059635070" HGAP="-230" ID="ID_480280779" MODIFIED="1280060327597" POSITION="right" TEXT="Inf&#xf3;s" VSHIFT="30">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1280089923527" ID="ID_15026989" MODIFIED="1280089996195" TEXT="Az &#xe9;rt&#xe9;krendembe beleillik-e a c&#xe9;g &#xe9;rt&#xe9;krendje?"/>
+<node CREATED="1280054103743" ID="ID_1586531280" MODIFIED="1280054113595" TEXT="Nekem val&#xf3; ez?"/>
+<node CREATED="1280059654478" ID="ID_1326010104" MODIFIED="1280059659546" TEXT="J&#xf3;l &#xe9;rezn&#xe9;m itt magam?"/>
+<node CREATED="1280058643630" ID="ID_1312605624" MODIFIED="1280058648602" TEXT="Mi&#xe9;rt j&#xf3; ez nekem?"/>
+<node CREATED="1280059865278" ID="ID_1816448967" MODIFIED="1280059875082" TEXT="Hol tudom feltenni a k&#xe9;rd&#xe9;seimet?"/>
+<node CREATED="1280059875406" ID="ID_1635729711" MODIFIED="1280059881802" TEXT="Hol tudok jelentkezni?"/>
+<node CREATED="1280058786926" ID="ID_1202505890" MODIFIED="1280059919978" TEXT="Szeretn&#xe9;m tudni, milyen k&#xf6;telezetts&#xe9;geim lesznek"/>
+<node CREATED="1280059368238" ID="ID_186193735" MODIFIED="1280059935050" TEXT="&#xc9;rt&#xe9;kelem, ha &#xe9;rdemben meghallgatnak"/>
+<node CREATED="1280089778903" ID="ID_1273399641" MODIFIED="1280089793954" TEXT="Mit kell nekem tudnom, hogy bel&#xe9;phessek?"/>
+<node CREATED="1280089806423" ID="ID_1564190793" MODIFIED="1280089883107" TEXT="Hogyan seg&#xed;ti ez a karrieremet?"/>
+<node CREATED="1280089884663" ID="ID_261888777" MODIFIED="1280089904467" TEXT="Meg&#xe9;ri-e ez nekem p&#xe9;nz&#xfc;gyileg?"/>
+<node CREATED="1280090106487" ID="ID_962674373" MODIFIED="1280090116722" TEXT="Milyen tov&#xe1;bbl&#xe9;p&#xe9;si lehet&#x151;s&#xe9;geim vannak?"/>
+</node>
+<node COLOR="#669900" CREATED="1280059289951" ID="ID_523030219" MODIFIED="1280088827483" POSITION="left" TEXT="Admin">
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1280054980319" ID="ID_1563234442" MODIFIED="1280054998891" TEXT="Tudjuk sz&#xe1;molni, ki inf&#xf3;s &#xe9;s ki j&#xf6;n c&#xe9;gt&#x151;l"/>
+<node CREATED="1280056192383" ID="ID_886894293" MODIFIED="1280056197019" TEXT="Szeretn&#xe9;nk visszajelz&#xe9;seket kapni"/>
+</node>
+</node>
+</map>
